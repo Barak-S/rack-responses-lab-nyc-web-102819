8cfd1959a0a683ab7f6e9a7638613ed73a72dceb
Barak Saidoff <barak.saidoff@gmail.com>
2019-11-18 13:06:03 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+    def call(new)
+        resp = Rack::Response.new
+        time = Time.now.hour
+        resp.write time
+
+        if time < 12
+            resp.write "Good Morning!"
+        else
+            resp.write "Good Afternoon"
+        end
+
+
+
+        resp.finish
+    end
+    
+end
